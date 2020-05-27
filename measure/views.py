06f83d8c8056927ba829186dbab9338f6a76f0b8
lalzatem <37983508+lalzatem@users.noreply.github.com>
2020-05-27 16:38:06 -0500

    actualizacion 3

--- measure/views.py
+++ measure/views.py
@@ -16,12 +16,15 @@ def measure(request):
         if value:
             # Crea el json para realizar la petición POST al Web Service
             args = {'type': '%', 'value': value,'longitud':longitud,'latitud':latitud,"tipo_de_terreno":tipo_de_terreno}
-            response = requests.post('http://127.0.0.1:8000/measure/', args)
+            #response = requests.post('http://127.0.0.1:8000/measure/', args)
+            response = requests.post('http://pi1-eafit-lalzate.azurewebsites.net/measures/', args) 
+
             # Convierte la respuesta en JSON
             measure_json = response.json()
 
     # Realiza una petición GET al Web Services
-    response = requests.get('http://127.0.0.1:8000/measure/')
+    #response = requests.get('http://127.0.0.1:8000/measure/')
+    response = requests.get('http://pi1-eafit-lalzate.azurewebsites.net/measures/')
     # Convierte la respuesta en JSON
     measures = response.json()
     # Rederiza la respuesta en el template measure
